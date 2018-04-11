57716c4bf05db5636d9c997d75eaa9d736e4e610
przemekNachel <przemekNachel@gmail.com>
2018-04-11 20:49:23 +0200

    Add hello message in Login

---
+++ src/main/java/App.java
@@ -0,0 +1,24 @@
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+
+
+public class App {
+
+    public static void main(String[] args) {
+
+        HttpServer server;
+        try {
+            server = HttpServer.create(new InetSocketAddress(8000), 0);
+            server.createContext("/", new Login());
+            server.createContext("/login", new Login());
+            server.setExecutor(null);
+            server.start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+}
---
+++ src/main/java/Login.java
@@ -0,0 +1,24 @@
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+public class Login implements HttpHandler {
+
+    @Override
+    public void handle(HttpExchange httpExchange) throws IOException {
+        String response = "Hello";
+//        String method = httpExchange.getRequestMethod();
+//        if(method.equals("POST")) {
+//            InputStreamReader isr = new InputStreamReader(httpExchange.getRequestBody(), "utf-8");
+//            BufferedReader br = new BufferedReader(isr);
+//            formData = br.readLine();
+//            addEntry();
+//        }
+        httpExchange.sendResponseHeaders(200, response.length());
+        OutputStream os = httpExchange.getResponseBody();
+        os.write(response.getBytes());
+        os.close();
+    }
+}
