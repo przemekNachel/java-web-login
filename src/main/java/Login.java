1ad012f477b8c97e728924619ea4822a198e826e
przemekNachel <przemekNachel@gmail.com>
2018-04-12 21:07:42 +0200

    Add fields in login class

--- src/main/java/Login.java
+++ src/main/java/Login.java
@@ -1,25 +1,53 @@
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 
-import java.io.IOException;
-import java.io.OutputStream;
+import java.io.*;
+import java.net.URLDecoder;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Login implements HttpHandler {
 
+    private HttpExchange httpExchange;
+    private String requestMethod;
+    private Map<String, String> formData;
+    private String response;
+
     @Override
     public void handle(HttpExchange httpExchange) throws IOException {
-        String response = "Hello ";
-        response += new Session("Przemek").getSessionId();
-//        String method = httpExchange.getRequestMethod();
-//        if(method.equals("POST")) {
-//            InputStreamReader isr = new InputStreamReader(httpExchange.getRequestBody(), "utf-8");
-//            BufferedReader br = new BufferedReader(isr);
-//            formData = br.readLine();
-//            addEntry();
-//        }
+
+        this.httpExchange = httpExchange;
+        this.requestMethod = httpExchange.getRequestMethod();
+
+        makeResponse();
+        sendResponse();
+    }
+
+    private void makeResponse() throws IOException{
+        if (requestMethod.equals("POST")) {
+            getFormData();
+        } else if (requestMethod.equals("GET")) {
+            response = new Session("Przemek").getSessionId();
+        }
+    }
+
+    private void sendResponse() throws IOException{
         httpExchange.sendResponseHeaders(200, response.length());
         OutputStream os = httpExchange.getResponseBody();
         os.write(response.getBytes());
         os.close();
+    }
+
+    private void getFormData() throws IOException {
+        InputStreamReader isr = new InputStreamReader(httpExchange.getRequestBody(), "utf-8");
+        String formDataString = new BufferedReader(isr).readLine();
+        if (formDataString != null) {
+            Map<String, String> map = new HashMap<>();
+            for (String pair : formDataString.split("&")) {
+                String[] keyValue = pair.split("=");
+                map.put(keyValue[0], new URLDecoder().decode(keyValue.length > 1 ? keyValue[1] : "", "UTF-8"));
+            }
+            formData = map;
+        }
     }
 }
